f754bd843cc75f02ca981d4fd4da55baf8c120d6
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-02 21:48:39 -0500

    \Total:                   0.0 of 70.0\

--- command.hh
+++ command.hh
@@ -12,7 +12,7 @@ struct Command {
   std::string *_errFile;
   bool _append;
   bool _background;
-  std::string *_errorFlag;
+  std::string _errorFlag;
 
   Command();
   void insertSimpleCommand(SimpleCommand *simpleCommand);
