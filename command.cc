16fcc1f4f72aad54c953f24a04be182b98fbb7fd
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-04 15:51:36 -0500

    \Total:                   0.0 of 70.0\

--- command.cc
+++ command.cc
@@ -174,7 +174,7 @@ void Command::execute() {
       if (ret == 0) {
         SimpleCommand *current_command = _simpleCommands.at(i);
 
-        execvp(current_command->_arguments.at(0), current_command->_arguments);
+        execvp(current_command->_arguments[0], current_command->_arguments);
         perror("execvp");
         exit(1);
       }
