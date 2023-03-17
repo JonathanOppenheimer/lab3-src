f8be47f83a75cadf21b9e222e5ebb9dde5299918
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-16 22:57:43 -0400

    \Total:                   21.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -876,7 +876,7 @@ YY_RULE_SETUP
 {
   std::string* trimmed = new std::string(yytext);
   trimmed = trimmed->substr(1, trimmed->size() - 2);
-  yylval.cpp_string = trimmed;
+  yylval.string = trimmed->c_str();
   return WORD;
 }
 	YY_BREAK
