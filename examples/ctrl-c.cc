940f6dd51771187681be4396741a58e6fd4dafb8
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-16 21:02:07 -0400

    \Total:                   21.0 of 70.0\

--- examples/ctrl-c.cc
+++ examples/ctrl-c.cc
@@ -3,45 +3,38 @@
 // Example of how to ignore ctrl-c
 //
 
-#include <stdio.h>
 #include <signal.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+extern "C" void disp(int sig) { fprintf(stderr, "\nsig:%d      Ouch!\n", sig); }
 
-extern "C" void disp( int sig )
-{
-	fprintf( stderr, "\nsig:%d      Ouch!\n", sig);
-}
+int main() {
+  printf("Type ctrl-c or \"exit\"\n");
 
-int main()
-{
-	printf( "Type ctrl-c or \"exit\"\n");
-    
-    struct sigaction sa;
-    sa.sa_handler = disp;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
+  struct sigaction sa;
+  sa.sa_handler = disp;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
 
-    if(sigaction(SIGINT, &sa, NULL)){
-        perror("sigaction");
-        exit(2);
+  if (sigaction(SIGINT, &sa, NULL)) {
+    perror("sigaction");
+    exit(2);
+  }
+
+  for (;;) {
+
+    char s[20];
+    printf("prompt>");
+    fflush(stdout);
+    fgets(s, 20, stdin);
+
+    if (!strcmp(s, "exit\n")) {
+      printf("Bye!\n");
+      exit(1);
     }
+  }
 
-	for (;;) {
-		
-		char s[ 20 ];
-		printf( "prompt>");
-		fflush( stdout );
-		fgets( s, 20, stdin );
-
-		if ( !strcmp( s, "exit\n" ) ) {
-			printf( "Bye!\n");
-			exit( 1 );
-		}
-	}
-
-	return 0;
+  return 0;
 }
-
-
--- shell.cc
+++ shell.cc
@@ -1,5 +1,6 @@
 #include <cstdio>
 
+#include <signal.h>
 #include <unistd.h>
 
 #include "shell.hh"
@@ -13,7 +14,21 @@ void Shell::prompt() {
   }
 }
 
+extern "C" void sigIntHandler(int sig) {
+  fprintf(stderr, "\nsig:%d      Ouch!\n", sig);
+}
+
 int main() {
+  struct sigaction signalAction;
+  signalAction.sa_handler = sigIntHandler;
+  sigemptyset(&signalAction.sa_mask);
+  signalAction.sa_flags = SA_RESTART;
+  int error = sigaction(SIGINT, &signalAction, NULL);
+  if (error) {
+    perror("sigaction");
+    exit(2);
+  }
+
   Shell::prompt();
   yyparse();
 }
