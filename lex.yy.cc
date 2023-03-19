9b57069b75919316c8dee32ae94b50062fd27c18
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-19 02:13:30 -0400

    \test_background Passed 2/2\

--- lex.yy.cc
+++ lex.yy.cc
@@ -947,6 +947,7 @@ YY_RULE_SETUP
         int next_quote = buffer.find('"', i+1);
         if(next_quote == std::string::npos && buffer[next_quote - 1] != '\\') {
           buffer.erase(i,1);
+          trimmed.clear();
           BEGIN(quotes);
           break;
         } else {
@@ -973,17 +974,17 @@ YY_RULE_SETUP
 /* Invalid character in input */
 case 16:
 YY_RULE_SETUP
-#line 149 "shell.l"
+#line 150 "shell.l"
 {
   return NOTOKEN;
 }
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 153 "shell.l"
+#line 154 "shell.l"
 ECHO;
 	YY_BREAK
-#line 987 "lex.yy.cc"
+#line 988 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(quotes):
 case YY_STATE_EOF(command):
@@ -2048,4 +2049,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 153 "shell.l"
+#line 154 "shell.l"
