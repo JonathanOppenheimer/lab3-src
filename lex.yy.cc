64773582cbca69a6ba32cd8753cf009ceff2ef14
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-19 02:57:45 -0400

    \Total:                   24.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -941,7 +941,7 @@ YY_RULE_SETUP
      */
     for(size_t i = 0; i < buffer.size(); ++i) {
       if(buffer[i] == '"') {
-        BEGIN(quotes)
+        BEGIN(quotes);
         /* int next_quote = buffer.find('"', i+1);
         if(next_quote == std::string::npos && buffer[next_quote - 1] != '\\') {
           buffer.erase(i,1);
