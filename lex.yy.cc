da0aeefce35fde4e0069d5cc55d7837f28b9708f
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-19 01:44:29 -0400

    \Total:                   24.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -834,8 +834,8 @@ YY_RULE_SETUP
     buffer += yytext;
     buffer.erase(buffer.rfind('"'), 1); /* Delete the trailing " */
     boost::trim(buffer);
-    yylval.cpp_string = new std::string(buffer.c_str());
-    printf(buffer);
+    yylval.cpp_string = new std::string(buffer);
+    printf(buffer.c_str());
     return WORD;
   }
 	YY_BREAK
