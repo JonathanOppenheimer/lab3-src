a905c64ba7e3dcf4dea0b0a024f4615ac17a5bdf
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-19 02:26:31 -0400

    \Total:                   24.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -944,6 +944,7 @@ YY_RULE_SETUP
         int next_quote = buffer.find('"', i+1);
         if(next_quote == std::string::npos && buffer[next_quote - 1] != '\\') {
           buffer.erase(i,1);
+          buffer.clear();
           BEGIN(quotes);
           break;
         } else {
@@ -966,17 +967,17 @@ YY_RULE_SETUP
 /* Invalid character in input */
 case 16:
 YY_RULE_SETUP
-#line 142 "shell.l"
+#line 143 "shell.l"
 {
   return NOTOKEN;
 }
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 146 "shell.l"
+#line 147 "shell.l"
 ECHO;
 	YY_BREAK
-#line 980 "lex.yy.cc"
+#line 981 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(quotes):
 case YY_STATE_EOF(command):
@@ -2041,4 +2042,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 146 "shell.l"
+#line 147 "shell.l"
