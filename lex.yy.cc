fc5d7aada2e4f5c1094a740760801310c797060f
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-19 02:29:29 -0400

    \Total:                   24.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -940,12 +940,16 @@ YY_RULE_SETUP
      * to close it.
      */
     for(size_t i = 0; i < buffer.size(); ++i) {
+      if(buffer[i] == '>') {
+        yylval.cpp_string = new std::string(buffer);
+        return WORD;
+      }
+
       if(buffer[i] == '"') {
         int next_quote = buffer.find('"', i+1);
         if(next_quote == std::string::npos && buffer[next_quote - 1] != '\\') {
           buffer.erase(i,1);
-          BEGIN(quotes);
-          break;
+          return BEGIN(quotes);
         } else {
           buffer.erase(i, 1); /* Delete the trailing " */
           buffer.erase(next_quote-1, 1); /* Delete the trailing " */
@@ -966,17 +970,17 @@ YY_RULE_SETUP
 /* Invalid character in input */
 case 16:
 YY_RULE_SETUP
-#line 142 "shell.l"
+#line 146 "shell.l"
 {
   return NOTOKEN;
 }
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 146 "shell.l"
+#line 150 "shell.l"
 ECHO;
 	YY_BREAK
-#line 980 "lex.yy.cc"
+#line 984 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(quotes):
 case YY_STATE_EOF(command):
@@ -2041,4 +2045,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 146 "shell.l"
+#line 150 "shell.l"
