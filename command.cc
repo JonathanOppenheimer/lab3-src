7871acb7351fe589a233479e59c859b7b26a2b99
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-21 00:24:36 -0400

    \Total:                   28.5 of 70.0\

--- command.cc
+++ command.cc
@@ -223,7 +223,6 @@ void Command::execute() {
         if (setenv(argv[1], argv[2], 1)) {
           perror("setenv");
         }
-        execute();
       }
 
       /* ************************************************ */
