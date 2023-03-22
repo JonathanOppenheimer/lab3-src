0fe0fef61daa678b7ad7c02cd738c66e6a70cec3
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-22 15:16:29 -0400

    \Total:                   38.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -2201,7 +2201,7 @@ void yyfree (void * ptr )
 int set_source() {
   if(isatty(0)) {
     yyparse();
-    YY_BUFFER_STATE cur = YY_CURRENT_BUFFER;
+    // YY_BUFFER_STATE cur = YY_CURRENT_BUFFER;
     YY_BUFFER_STATE source = yy_scan_string("source .shellrc\0\0");
     yyparse();
     // yy_delete_buffer(source);
