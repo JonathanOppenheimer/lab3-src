f88719ae864509f71d67279175ee9953c441c108
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-22 00:18:24 -0400

    \Total:                   34.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1034,8 +1034,6 @@ YY_RULE_SETUP
       // Where to keep the results of the regex
       std::smatch matches;
 
-      std::cout << shell_location;
-
       if(std::regex_search(buffer, standard)) {
         while(std::regex_search(buffer, matches, standard)) {
           // First check if the environment variable exists
@@ -1062,6 +1060,7 @@ YY_RULE_SETUP
         while(std::regex_search(buffer, matches, standard)) {
           buffer = std::regex_replace(buffer, name_shell, shell_location);
         }
+        std::cout << shell_location;
         // matches = nullptr_t; // Empty matches
       } else {
         std::cout << buffer + ": bad substitution\n";
@@ -1104,7 +1103,7 @@ YY_RULE_SETUP
 /* Invalid character in input */
 case 21:
 YY_RULE_SETUP
-#line 242 "shell.l"
+#line 241 "shell.l"
 {
   /* return NOTOKEN; */
 }
@@ -1113,7 +1112,7 @@ YY_RULE_SETUP
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(quotes):
 case YY_STATE_EOF(manual_source):
-#line 247 "shell.l"
+#line 246 "shell.l"
 {
   yypop_buffer_state();
   if (!YY_CURRENT_BUFFER) {
@@ -1123,10 +1122,10 @@ case YY_STATE_EOF(manual_source):
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 254 "shell.l"
+#line 253 "shell.l"
 ECHO;
 	YY_BREAK
-#line 1130 "lex.yy.cc"
+#line 1129 "lex.yy.cc"
 
 	case YY_END_OF_BUFFER:
 		{
@@ -2187,4 +2186,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 254 "shell.l"
+#line 253 "shell.l"
