89274ad6a7ad7e6851cb86a7f6ed2e9cf454d33e
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-21 20:11:04 -0400

    \Total:                   30.5 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1035,7 +1035,7 @@ YY_RULE_SETUP
       std::smatch matches;
 
       if(std::regex_search(buffer, standard)) {
-        buffer = std::regex_replace(buffer, standard, getenv("HOME"));
+        buffer = std::regex_replace(buffer, standard, getenv("a"));
         /* while(std::regex_search(buffer, matches, standard)) {
           std::cout << matches[0] << std::endl;
           buffer = std::regex_replace(buffer, )
