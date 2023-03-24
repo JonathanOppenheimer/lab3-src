60446d5fffb0fc17634403a17d10ce2c67f16f22
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-24 17:53:51 -0400

    \Total:                   40.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1028,7 +1028,7 @@ YY_RULE_SETUP
   char char_buffer[1];
   int is_eof = read(pout[0], char_buffer, 1);
   while(is_eof) {
-    if(char_buffer != '\n') {
+    if(*char_buffer != '\n') {
       results.push_back(char_buffer);
     }
     fprintf(stderr, "%s\n", char_buffer);
