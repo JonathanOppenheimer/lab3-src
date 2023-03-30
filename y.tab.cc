a170f933d75d8576e827fe5ff3652b44620721d0
Jonathan Aimison Oppenheimer <joppenhe@mc18.cs.purdue.edu>
2023-03-30 01:04:09 -0400

    \Total:                   56.0 of 70.0\

--- y.tab.cc
+++ y.tab.cc
@@ -1393,7 +1393,7 @@ yyreduce:
     // Add the entries as arguements if matches found, otherwise, just insert the original arg
     if(matching_args.size() > 0 ) {
       for (int i = 0; i < matching_args.size(); i++) {
-        Command::_currentSimpleCommand->insertArgument(new std::string(matching_args[i]));
+        Command::_currentSimpleCommand->insertArgument(matching_args[i]);
       }
     } else {
       Command::_currentSimpleCommand->insertArgument( (yyvsp[0].cpp_string) );
