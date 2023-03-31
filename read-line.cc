eb736095c26b7b91700e07863aacf49c9a568f3e
Jonathan Aimison Oppenheimer <joppenhe@escher21.cs.purdue.edu>
2023-03-31 14:51:51 -0400

    \Total:                   70.0 of 70.0\

--- read-line.cc
+++ read-line.cc
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <vector>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,12 +16,12 @@
 #define MAX_BUFFER_LINE 2048
 
 extern void tty_raw_mode(void);
+void insertChar(int, char);
 void wipeLine(char);
 
-// Buffer where line is stored
-int line_length;
-int total_chars;
-char line_buffer[MAX_BUFFER_LINE];
+char line_buffer[MAX_BUFFER_LINE]; // Buffer where line is stored
+int line_pos;                      // Where in the buffer we are
+int total_chars;                   // Total number of characters in the buffer
 
 // Simple history array
 // This history does not change.
@@ -47,7 +48,7 @@ char *read_line() {
   // Set terminal in raw mode
   tty_raw_mode();
 
-  line_length = 0;
+  line_pos = 0;
 
   // Read one line until enter is typed
   while (1) {
@@ -60,18 +61,23 @@ char *read_line() {
     if ((in_char >= 32) && (in_char != 127)) {
       // Printable character that is not delete
 
-      // Write depending on where the cursor is
-      // Write the character out
-      write(1, &in_char, 1);
+      // Check whether we are writing at the end of the line, or within the line
+      if (line_pos == total_chars) { // At the start of the line
+
+        // Write the character out
+        write(1, &in_char, 1);
+        // add char to buffer.
+        line_buffer[line_pos] = in_char;
+        line_pos++;
+        total_chars++;
+      } else { // We're somewhere within the line
+      }
 
       // If max number of character reached return.
-      if (line_length == MAX_BUFFER_LINE - 2)
+      if (line_pos == MAX_BUFFER_LINE - 2)
         break;
 
-      // add char to buffer.
-      line_buffer[line_length] = in_char;
-      line_length++;
-      total_chars++;            // Increment total_chars
+      // Increment total_chars
     } else if (in_char == 10) { // <Enter> - return line
       // Print newline
       write(1, &in_char, 1);
@@ -84,7 +90,7 @@ char *read_line() {
       break;
     } else if (in_char == 127) { // <Backspace> - remove previous character read
       // Only delete if the line length is longer than 0
-      if (line_length > 0) {
+      if (line_pos > 0) {
         // Go back one character
         in_char = 8;
         write(1, &in_char, 1);
@@ -98,7 +104,7 @@ char *read_line() {
         write(1, &in_char, 1);
 
         // Remove one character from buffer
-        line_length--;
+        line_pos--;
         total_chars--; // Decrement total_chars
       }
     } else if (in_char == 27) {
@@ -123,30 +129,30 @@ char *read_line() {
 
         // Copy line from history
         strcpy(line_buffer, history[history_index]);
-        line_length = strlen(line_buffer);
+        line_pos = strlen(line_buffer);
         history_index = (history_index + 1) % history_length;
 
         // echo line
-        write(1, line_buffer, line_length);
+        write(1, line_buffer, line_pos);
       } else if ((ch1 == 91) && (ch2 == 66)) { // Down arrow
       } else if ((ch1 == 91) && (ch2 == 67)) { // Right arrow
                                                //
       } else if ((ch1 == 91) && (ch2 == 68)) { // Left arrow
         // Only go back if the current line position is greater than 0
-        if (line_length > 0) {
+        if (line_pos > 0) {
           // Go back one character
           in_char = 8;
           write(1, &in_char, 1);
-          line_length--;
+          line_pos--;
         }
       }
     }
   }
 
   // Add eol and null char at the end of string before returning
-  line_buffer[line_length] = 10;
-  line_length++;
-  line_buffer[line_length] = 0;
+  line_buffer[line_pos] = 10;
+  line_pos++;
+  line_buffer[line_pos] = 0;
 
   return line_buffer;
 }
@@ -154,19 +160,19 @@ char *read_line() {
 void wipeLine(char in_char) {
   // Move to start of line by printing backspaces
   int i = 0;
-  for (i = 0; i < line_length; i++) {
+  for (i = 0; i < line_pos; i++) {
     in_char = 8;
     write(1, &in_char, 1);
   }
 
   // Print spaces on top to erase old line
-  for (i = 0; i < line_length; i++) {
+  for (i = 0; i < line_pos; i++) {
     in_char = ' ';
     write(1, &in_char, 1);
   }
 
   // Move to start of line by printing backspaces
-  for (i = 0; i < line_length; i++) {
+  for (i = 0; i < line_pos; i++) {
     in_char = 8;
     write(1, &in_char, 1);
   }
