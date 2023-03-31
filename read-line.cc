c4ab8c0f921595d88eaa3ad778dab46c7551cd45
Jonathan Aimison Oppenheimer <joppenhe@mc18.cs.purdue.edu>
2023-03-31 17:44:13 -0400

    \Total:                   51.0 of 70.0\

--- read-line.cc
+++ read-line.cc
@@ -66,7 +66,7 @@ char *read_line() {
       // Check whether we are writing at the end of the line, or within the line
       if (line_pos == total_chars) { // At the start of the line
         insertChar(in_char);
-        moveCursorRight(line_pos, total_chars);
+        moveCursorRight(line_pos, line_pos + 1);
 
       } else { // We're somewhere within the line so need to adjust the cursor
         insertChar(in_char);
