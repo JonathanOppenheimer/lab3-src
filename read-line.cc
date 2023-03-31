1d4f0cfa60ca17217d14dd531ccfb0ffc66ff897
Jonathan Aimison Oppenheimer <joppenhe@mc18.cs.purdue.edu>
2023-03-31 18:01:36 -0400

    \Total:                   51.0 of 70.0\

--- read-line.cc
+++ read-line.cc
@@ -130,7 +130,7 @@ char *read_line() {
 
       if ((ch1 == 91) && (ch2 == 65)) { // Up arrow - print next line in history
         // Wipe current line
-        // moveCursorRight(line_pos, total_chars);
+        moveCursorRight(line_pos, total_chars);
         wipeLine(0, total_chars);
 
         // Copy line from history
