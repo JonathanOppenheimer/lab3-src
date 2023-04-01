082067f2b0e4dcc3c5cdd101da9b3d5cf98fdcd8
JonathanOppenheimer <joppenhe@purdue.edu>
2023-03-31 22:20:10 -0400

    \Total:                   51.0 of 70.0\

--- read-line.cc
+++ read-line.cc
@@ -102,13 +102,10 @@ char *read_line() {
         delete_char(line_pos); // Delete the character
         total_chars--;
 
+        moveCursorLeft(1);
         wipeLine(line_pos, total_chars); // Wipe all after current character
         moveCursorRight(line_pos, total_chars + 1); // Rewrite partial new line
         moveCursorLeft(total_chars - line_pos);     // Move cursor to prev pos
-
-        // wipeLine(line_pos, total_chars);
-        // moveCursorRight(line_pos, total_chars + 1);
-        // moveCursorLeft(total_chars - line_pos);
       }
     } else if (in_char == 27) {
       /* Escape sequence detected - read two chararacterss more to determine
