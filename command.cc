9aaec73f92ad3755c3acfe88700a7de4f4d0dd89
JonathanOppenheimer <joppenhe@purdue.edu>
2023-04-01 18:09:29 -0400

    \Total:                   70.0 of 70.0\

--- command.cc
+++ command.cc
@@ -319,6 +319,7 @@ void Command::execute() {
     if (!_background) {
       waitpid(ret, &status, 0);               // Wait for last command
       last_return_code = WEXITSTATUS(status); // Update global last return code
+      std::cout << last_return_code << "\n";
     } else {
       background_pids.insert(
           ret); // Add background PID to global vector for zombie elimination
