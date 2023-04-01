bebf6c734fa12c70d2a261f1a22d76c67b1dd3f9
Jonathan Aimison Oppenheimer <joppenhe@mc19.cs.purdue.edu>
2023-04-01 01:25:19 -0400

    \Total:                   51.0 of 70.0\

--- read-line.cc
+++ read-line.cc
@@ -106,7 +106,7 @@ char *read_line() {
         total_chars--;
 
         wipeLine(line_pos, total_chars + 1); // Wipe all after current character
-        moveCursorRight(line_pos, total_chars + 1); // Rewrite partial new line
+        moveCursorRight(line_pos, total_chars + 2); // Rewrite partial new line
         moveCursorLeft(total_chars - line_pos);     // Move cursor to prev pos
       }
     } else if (in_char == 10) { // <Enter> - return line
