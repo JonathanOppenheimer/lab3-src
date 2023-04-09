652a35456fa96c51add582874c7153223c69061b
JonathanOppenheimer <joppenhe@purdue.edu>
2023-04-09 02:39:11 -0400

    \Total:                   70.0 of 70.0\

--- read-line.cc
+++ read-line.cc
@@ -166,13 +166,11 @@ char *read_line() {
         std::string match = *matching_args.at(0);
         int last_matching_index = 0;
         for (int i = last_space + 1; i < total_chars; i++) {
-          std::cout << "\n"
-                    << match[i - last_space + 1] << "  " << line_buffer[i]
-                    << "\n";
+          std::cout << "\n" << line_buffer[i] << "\n";
 
           last_matching_index = i - last_space;
           if (match[i - last_space + 1] != line_buffer[i]) {
-            break;
+            // break;
           }
         }
 
